d7b9fa3f3ed074561ac6b7554a06ff322605094c
Cameron Delano <knowbase@icloud.com>
2023-03-01 07:49:50 -0800

    dsg fix

--- modules/infra/security_groups.tf
+++ modules/infra/security_groups.tf
@@ -2,7 +2,7 @@
 resource "aws_default_security_group" "restrict_dsg" {
   vpc_id = module.vpc.vpc_id
   tags = {
-    Name  = format("%s-default-sg-%s", var.project_prefix, random_id.build_suffix.hex)
+    Name  = format("%s-default-sg-%s", var.project_prefix, var.build_suffix)
     Owner = var.resource_owner
   }
 }
