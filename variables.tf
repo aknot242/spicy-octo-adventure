cd6b3621ca52eabdc40d6a8df43f7a5ca623c0ac
Cameron Delano <knowbase@icloud.com>
2023-03-01 07:06:40 -0800

    spcy test 4

--- variables.tf
+++ variables.tf
@@ -69,10 +69,12 @@ variable "vpc_main_route_table_id" {
   type = string
   description = "VPC route table ID"
 }
+/*
 variable "public_subnet_ids" {
   type = list(any)
   description = "Provide if infra is set to false and create_nat_gateway is set to true"
 }
+*/
 variable "admin_src_addr" {
   type        = string
   description = "Allowed Admin source IP prefix"
